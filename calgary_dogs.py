7a511f888619a7606a37ae9d7ca08c66b533802a
meluxmeme <meluxmeme@gmail.com>
2024-06-09 13:59:55 -0600

    Stage 2: user input part is done

--- calgary_dogs.py
+++ calgary_dogs.py
@@ -1,17 +1,31 @@
 # calgary_dogs.py
-# AUTHOR NAME
+# AUTHOR NAME: Laxmi Paudel
 #
 # A terminal-based application for computing and printing statistics based on given input.
 # Detailed specifications are provided via the Assignment 4 README file.
 # You must include the main listed below. You may add your own additional classes, functions, variables, etc. 
 # You may import any modules from the standard Python library.
 # Remember to include docstrings and comments.
-
+import numpy as np
+import pandas as pd
 def main():
 
-    # Import data here
-
+    dataFrame = pd.read_excel('CalgaryDogBreeds.xlsx')
     print("ENSF 692 Dogs of Calgary")
+    dataFrame.set_index(['Breed','Year','Month'],inplace=True)
+
+    #print(dataFrame)
+    while True:
+        try:
+            userInputDog = input("Enter Dog breed name:  ")
+            if (userInputDog in dataFrame.index.get_level_values('Breed')):
+                print("Dog breed found in the database")
+                break
+            else:
+                raise  ValueError("The dog breed is not found, enter again")
+        
+        except ValueError as e:
+            print(e)
 
     # User input stage
 
